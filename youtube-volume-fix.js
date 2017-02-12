329967c358df0b5709d7fa1f55497987faf00422
Connor Linfoot <connorlinfoot@me.com>
2017-02-12 02:54:07 +0000

    Initial commit

---
+++ youtube-volume-fix.js
@@ -0,0 +1,27 @@
+var video_element = document.getElementsByClassName( 'html5-main-video' );
+var video = null;
+
+if( video_element.length == 1 ) {
+	// Assuming YouTube :P (I'm lazy)
+	video = video_element[0];
+	video.volume = video.volume * 2;
+
+	var we_updated_volume = false;
+	video.onvolumechange = function(event) {
+		if( we_updated_volume ) {
+			we_updated_volume = false;
+			return;
+		}
+		var volume = video.volume;
+		if( volume > 0.5 ) {
+			return;
+		}
+		we_updated_volume = true;
+		video.volume = volume * 2;
+	};
+
+	// setInterval(function() {
+	// 	console.log( 'Current volume ' + video.volume );
+	// }, 500);
+
+}
